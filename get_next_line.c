c6fedd0ebca05807d92c2b3d75e7326c7e160cd0
saragar2 <garciamiguelsara@gmail.com>
2023-10-30 20:55:00 +0100

    en realidad es un asco pero funciona. Acuerdate de terminar el join, ahi esta la clave

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: saragar2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 18:20:28 by saragar2          #+#    #+#             */
-/*   Updated: 2023/10/28 18:05:47 by saragar2         ###   ########.fr       */
+/*   Updated: 2023/10/30 20:53:40 by saragar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,25 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	return (src_len);
 }
 
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*newstr;
+	int		size;
+
+	size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	newstr = malloc(size);
+	if (!newstr)
+		return (NULL);
+	while (*s1 != '\0')
+		*newstr++ = *s1++;
+	while (*s2 != '\0')
+		*newstr++ = *s2++;
+	*newstr = '\0';
+	newstr -= (size - 1);
+	return (newstr);
+}
+
 char *get_next_line(int fd)
 {
 	static char	*buf;
@@ -45,18 +64,24 @@ char *get_next_line(int fd)
 
 	i = 0;
 	j = -2;
+	buf = malloc(BUFFER_SIZE);
 	if (!buf)
 		read(fd, buf, BUFFER_SIZE);
 	while (buf[i] != '\n' && j != 0)
 	{
 		i++;
 		if ((i == BUFFER_SIZE || i == j) && buf[i] != '\n')
+		{
+			ft_strjoin()
+			i = 0;
 			j = read(fd, buf, BUFFER_SIZE);
+		}
 	}
-	aux = malloc(sizeof(char) * i);
+	aux = malloc(sizeof(char) * (i + 3));
 	if (!aux)
 		return (NULL);
-	ft_strlcpy(aux, buf, i);
+	ft_strlcpy(aux, buf, i + 2);
+	aux[++i] = '\0';
 	return (aux);
 }
 
@@ -64,6 +89,6 @@ int	main()
 {
 	int fd = open("prueba.txt", O_RDONLY);
 	printf("%s", get_next_line(fd));
-	//printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 	return (0);
 }
